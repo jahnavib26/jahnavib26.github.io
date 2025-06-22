c25e06f7ba5763b011329614a61b1bae8cb9ec50
Jahnavi Bollineni <jahnavibollineni@Jahnavis-MacBook-Air.local>
2025-06-22 17:52:42 -0400

    Updated Hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,35 +1,47 @@
-import memojiImage from '@/assets/images/memoji-computer.png';
-import Image from 'next/image';
-import ArrowDown from '@/assets/icons/arrow-down.svg'
+import memojiImage from "@/assets/images/memoji-computer.png";
+import Image from "next/image";
+import ArrowDown from '@/assets/icons/arrow-down.svg'; 
 
 export const HeroSection = () => {
-  return (
-  <div className = "py-32">
-    <div className = "container">
-    <div>
-      <Image 
-      src = {memojiImage} 
-      className = "size-[100px]" 
-      alt = "person"
-      />
-      <div>
-        <div></div>
-        <div>Available for new projects</div>
+  return ( 
+  <div>
+    <div className = "container mt-20">
+      <div className = "flex flex-col items-center">
+        <Image 
+          src = {memojiImage} 
+          className = "size-[100px]" 
+          alt="Person peeking behind laptop" 
+        />
+        <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+          <div className="bg-green-500 size-2.5 rounded-full"></div>
+          <div className="text-sm font-medium">
+            Available for new projects
+          </div>
+        </div>
       </div>
-    </div>
-      <h1>Hi, I'm Jahnavi</h1>
-      <p>This is a description about me</p>
-      <div>
-        <button>
-          <span>Explore My Work</span>
-          <ArrowDown/>
+
+      <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">
+        Building Exceptional User Experiences
+      </h1>
+
+      <p className="mt-4 text-center text-white/60">
+        I specialize in transforming designs into functional, high-performing
+        web applications. Let's discuss your next project.
+      </p>
+
+      <div className="flex flex-col items-center mt-8 gap-4">
+        <button className = "inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+          <span className = "font-semibold">Explore My Work</span>
+          <ArrowDown className="size-4" />
         </button>
-        <button>
+        <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
           <span>👋</span>
-          <span>Let's Connect</span>
+          <span className="font-semibold">Let's Connect</span>
         </button>
       </div>
+
     </div>
   </div>
   );
+
 };
